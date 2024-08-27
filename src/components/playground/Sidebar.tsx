29a0d1b6f00fbb3b286daa98082162f47363767b
Dhruv Atreja <dhruvatrejamain@gmail.com>
2024-08-27 18:40:39 +0530

    Animation for sidebar

--- src/components/playground/Sidebar.tsx
+++ src/components/playground/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Stream } from './Stream'
 import { GraphState } from './Playground'
 
@@ -8,9 +8,24 @@ interface SidebarProps {
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({ graphState, onClose }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    setIsOpen(true)
+  }, [])
+
+  const handleClose = () => {
+    setIsOpen(false)
+    setTimeout(onClose, 300) // Delay closing to allow animation to complete
+  }
+
   return (
-    <div className='fixed top-0 right-0 h-full w-1/3 bg-white shadow-lg overflow-y-auto z-50 transition-transform duration-300 ease-in-out bg-emerald-950'>
-      <button onClick={onClose} className='absolute top-4 right-4 text-gray-600 hover:text-gray-800'>
+    <div
+      className={`fixed top-0 right-0 h-full w-1/3  shadow-lg overflow-y-auto z-50 transition-all duration-300 ease-in-out bg-emerald-950 ${
+        isOpen ? 'translate-x-0' : 'translate-x-full'
+      }`}
+    >
+      <button onClick={handleClose} className='absolute top-4 right-4 text-white'>
         ✕
       </button>
       <div className='p-4'>
