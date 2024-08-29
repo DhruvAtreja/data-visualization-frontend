caa5499841fe9a638e80214f2011a5c2e41bd847
Dhruv Atreja <dhruvatrejamain@gmail.com>
2024-08-29 18:51:20 +0530

    upload button style

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -46,7 +46,7 @@ const UploadButton: React.FC<UploadButtonProps> = ({ onFileUpload, disabled }) =
       <button
         onClick={handleClick}
         disabled={disabled}
-        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+        className={`px-4 py-2 bg-white text-black rounded hover:bg-gray-200 transition-colors duration-300 ${
           disabled ? 'opacity-50 cursor-not-allowed' : ''
         }`}
       >
